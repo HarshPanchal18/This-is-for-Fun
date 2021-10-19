e1882a2e095a066d5763b0c480b6add525c307fb
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2021-10-19 14:02:53 +0530

    Add files via upload

---
+++ PrintCurrentFile.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+int main(void)
+{
+	FILE *fp;
+	char ch;
+	fp = fopen("counter.c", "r");
+	if (fp == NULL)
+	{
+		printf("Cannot open file");
+		exit(1);
+	}
+	while (1)
+	{
+		ch = fgetc(fp);
+		if (ch == EOF)
+			break;
+		printf("%c", ch);
+	}
+	printf("\n");
+	fclose(fp);
+	getch();
+}
