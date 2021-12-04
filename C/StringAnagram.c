cebbf2319c8aa4f03fdea325aa1d7d54a759686a
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2021-12-04 08:22:23 +0530

    Add files via upload

---
+++ C/StringAnagram.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int Anagram(char s1[], char s2[])
+{
+    int first[200] = {0};
+    int second[200] = {0};
+
+    int i = 0;
+    while (s1[i] != '\0')
+    {
+        first[s1[i]]++;
+        i++;
+    }
+    i = 0;
+    while (s2[i] != '\0')
+    {
+        second[s2[i]]++;
+        i++;
+    }
+
+    for (i = 0; i < 256; i++)
+        if (first[i] != second[i])
+            return 0;
+    return 1;
+}
+
+int main(void)
+{
+    char s1[50], s2[50];
+    int check;
+
+    printf("\nEnter String1: ");
+    gets(s1);
+
+    printf("\nEnter String2: ");
+    gets(s2);
+
+    check = Anagram(s1, s2);
+
+    if (check == 1)
+        printf("\n%s & %s are Anagram ", s1, s2);
+    else
+        printf("\n%s & %s are not Anagram ", s1, s2);
+}
+
+/*
+    Yeh => hYe
+*/
