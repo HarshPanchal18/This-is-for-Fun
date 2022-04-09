6d2ce696c00761b7047f4be13ef60cb454999dfb
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-04-09 12:24:55 +0530

    Create Doggy.c

---
+++ C/Doggy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n\n");
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("(_0_)\"\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("|_|_/=\\\\_|\n");
+    printf("\n\n");
+    printf("\n\n");
+}
+
+/*
+
+|\_/|     
+|q p|   /}
+(_0_)""""\
+|"^"`    |
+|_|_/=\\_|
+
+*/
