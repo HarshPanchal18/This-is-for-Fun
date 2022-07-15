d04fb083e7813c4411387ce74792c01204a08967
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-07-15 11:05:08 +0530

    Create ConvertToAscii.py

---
+++ Python/ConvertToAscii.py
@@ -0,0 +1,5 @@
+# Python code to get the ASCII value of the character
+char_input = input("Enter a character to print it's ASCII value: ")
+
+# printing the ASCII value, using the ord() function in python
+print("The ASCII value of ", char_input, "is ", ord(char_input))
