a77970bf47a343ac65fab1674ffd1fe33b87df08
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-10-09 15:16:02 +0530

    Create ladder.py

---
+++ Python/Turtle/ladder.py
@@ -0,0 +1,76 @@
+# Ladder Fun
+import turtle
+
+smart = turtle.Turtle()
+for i in range(4):
+    smart.forward(50)
+    smart.right(90)
+    smart.forward(50)
+    smart.left(90)
+
+smart.left(180)
+smart.forward(200)
+smart.right(90)
+smart.forward(200)
+smart.left(90)
+
+for i in range(4):
+    smart.forward(50)
+    smart.left(90)
+    smart.forward(50)
+    smart.right(90)
+
+smart.left(180)
+smart.forward(400)
+
+for i in range(5):
+    smart.left(90)
+    smart.forward(5)
+    smart.left(90)
+    smart.forward(400)
+    smart.right(90)
+    smart.forward(5)
+    smart.right(90)
+    smart.forward(400)
+
+smart.left(180)
+smart.forward(50)
+smart.right(90)
+
+for i in range(5):
+    smart.forward(5)
+    smart.left(90)
+    smart.forward(300)
+    smart.right(90)
+    smart.forward(5)
+    smart.right(90)
+    smart.forward(300)
+    smart.left(90)
+
+smart.left(90)
+smart.forward(50)
+smart.right(90)
+
+for i in range(5):
+    smart.forward(5)
+    smart.left(90)
+    smart.forward(200)
+    smart.right(90)
+    smart.forward(5)
+    smart.right(90)
+    smart.forward(200)
+    smart.left(90)
+
+smart.left(90)
+smart.forward(50)
+smart.right(90)
+
+for i in range(5):
+    smart.forward(5)
+    smart.left(90)
+    smart.forward(100)
+    smart.right(90)
+    smart.forward(5)
+    smart.right(90)
+    smart.forward(100)
+    smart.left(90)
